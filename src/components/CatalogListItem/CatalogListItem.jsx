3d6ed1647b253b1afe4465cf56cfd984887fae12
cristian-corbalan <ccorbalandw@gmail.com>
2024-07-23 20:10:25 -0300

    Create hover effect to items

--- src/components/CatalogListItem/CatalogListItem.jsx
+++ src/components/CatalogListItem/CatalogListItem.jsx
@@ -4,13 +4,15 @@ export default function CatalogListItem ({ item }) {
   const imageSrc = item.images['Poster Art'].url;
   return (
     <li className={styles.item}>
-      <div className={styles.imgContainer}>
-        <img src={imageSrc} alt={item.title} />
-      </div>
-      <div className={styles.content}>
-        <h2 className={styles.title}>{item.title}</h2>
-        <p className={styles.description}>{item.releaseYear}</p>
-      </div>
+      <a href="#" className={styles.link}>
+        <div className={styles.imgContainer}>
+          <img src={imageSrc} alt={item.title} />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>{item.title}</h2>
+          <p className={styles.description}>{item.releaseYear}</p>
+        </div>
+      </a>
     </li>
   );
 }
